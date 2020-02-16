6753ec40a525277071cfc333567f70fe5f0f04c3
DDH <ddhoang1993@gmail.com>
2020-02-16 15:00:12 -0800

    created a connection and connected it

--- employees.js
+++ employees.js
@@ -4,3 +4,16 @@ const mysql = require("mysql");
 const inquirer = require("inquirer");
 const consoleTable = require("console.table");
 
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "MyPassword",
+    database: "employees_db"
+});
+
+connection.connect(err => {
+    if (err) throw err;
+    prompt();
+});
+
