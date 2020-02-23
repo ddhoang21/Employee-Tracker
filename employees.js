3d19801a71ce5802f735dda0b25cddc141e09b69
DDH <ddhoang1993@gmail.com>
2020-02-23 13:39:04 -0800

    changed port

--- employees.js
+++ employees.js
@@ -6,7 +6,7 @@ const consoleTable = require("console.table");
 
 const connection = mysql.createConnection({
     host: "localhost",
-    port: 3306,
+    port: process.env.PORT || 3306,
     user: "root",
     password: "MyPassword",
     database: "employees_db"
